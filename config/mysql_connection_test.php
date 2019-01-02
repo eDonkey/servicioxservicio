06911f21bc42b6434c16a11d3b36822d65ced4ae
Axel Wdoviak <kooltheoutsider@gmail.com>
2019-01-02 15:45:23 -0300

    Update config/mysql_connection_test.php

--- config/mysql_connection_test.php
+++ config/mysql_connection_test.php
@@ -1,9 +1,9 @@
 <?php
 include("mysql_adapter.php");
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysqli_select_db($dbname) or die("Could not open the db '$dbname'");
+mysqli_select_db($connect, $dbname) or die("Could not open the db '$dbname'");
 $test_query = "SHOW TABLES FROM $dbname";
-$result = mysqli_query($test_query);
+$result = mysqli_query($connect, $test_query);
 $tblCnt = 0;
 while($tbl = mysqli_fetch_array($result)) {
   $tblCnt++;
