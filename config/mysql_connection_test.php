b3ba285b15bb378f674e91ae65cfc89760e962e0
Axel Wdoviak <kooltheoutsider@gmail.com>
2019-01-02 15:43:16 -0300

    fixing error

--- config/mysql_connection_test.php
+++ config/mysql_connection_test.php
@@ -1,11 +1,11 @@
 <?php
 include("mysql_adapter.php");
-$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysql_select_db($dbname) or die("Could not open the db '$dbname'");
+$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($dbname) or die("Could not open the db '$dbname'");
 $test_query = "SHOW TABLES FROM $dbname";
-$result = mysql_query($test_query);
+$result = mysqli_query($test_query);
 $tblCnt = 0;
-while($tbl = mysql_fetch_array($result)) {
+while($tbl = mysqli_fetch_array($result)) {
   $tblCnt++;
 }
 if (!$tblCnt) {
