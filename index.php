dac4cd4991259d0ef8760fa280d598d746d820da
Axel Wdoviak <kooltheoutsider@gmail.com>
2019-01-09 17:11:33 -0300

    optimizing css

--- index.php
+++ index.php
@@ -17,6 +17,34 @@ include("config/fbconfig.php");
             body {
                 background-color: #f1d849;
             }
+            .searchfield {
+                font-family: "Montserrat", sans-serif;
+                width: 30em;
+                height: 4em;
+                border: 0.1em solid #ccc;
+                border-radius: 0.5em;
+                background-color: #efefef;
+                padding: 0em 1em;
+                font-size: 1.2em;
+                box-shadow: 0em 0.1em 0.5em #ccc;
+            }
+            .searchbutton {
+                padding: 1px 2px 1px !important;
+                font-size: 12px !important;
+                background-color: #1B349F;
+                font-weight: bold;
+                text-shadow: 1px 1px #1B349F;
+                color: #ffffff;
+                border-radius: 100px;
+                -moz-border-radius: 100px;
+                -webkit-border-radius: 100px;
+                border: 1px solid #1B349F;
+                cursor: pointer;
+                box-shadow: 0 1px 0 rgba(255, 255, 255, 0.5) inset;
+                -moz-box-shadow: 0 1px 0 rgba(255, 255, 255, 0.5) inset;
+                -webkit-box-shadow: 0 1px 0 rgba(255, 255, 255, 0.5) inset;
+                width:80px;
+            }
         </style>
     </head>
     <body>
@@ -75,7 +103,7 @@ include("config/fbconfig.php");
         </div>
         <div id="searchcontainer" style="position:fixed;top: 50%;left: 50%;width:30em;height:12em;margin-top: -9em;margin-left: -15em;border: 1px solid #ccc;background-color: #f3f3f3;visibility:visible;">
             <form method="post" action="">
-                <p><input type="text" id="busqueda" name="busqueda" size="30" /><input type="submit" value="BUSCAR" style="padding: 1px 2px 1px !important;font-size: 12px !important;background-color: #1B349F;font-weight: bold;text-shadow: 1px 1px #1B349F;color: #ffffff;border-radius: 100px;-moz-border-radius: 100px;-webkit-border-radius: 100px;border: 1px solid #1B349F;cursor: pointer;box-shadow: 0 1px 0 rgba(255, 255, 255, 0.5) inset;-moz-box-shadow: 0 1px 0 rgba(255, 255, 255, 0.5) inset;-webkit-box-shadow: 0 1px 0 rgba(255, 255, 255, 0.5) inset;width:80px;" /></p>
+                <p><input type="text" id="busqueda" name="busqueda" size="30" class="searchfield" /><input type="submit" value="BUSCAR" class="searchbutton" /></p>
             </form>
         </div>
         <div id"logincontainer" style="position:fixed;top: 50%;left: 50%;width:30em;height:12em;margin-top: -9em;margin-left: -15em;border: 1px solid #ccc;background-color: #f3f3f3;visibility:hidden;">
