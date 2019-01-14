86b7275c1fda922dc3bff5276d634bec9d9aaa3c
Axel Wdoviak <kooltheoutsider@gmail.com>
2019-01-14 17:24:27 -0300

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ include("config/site_based_checks.php");
 $email = mysqli_real_escape_string($connect, $_POST['username']);
 $password = password_encrypt($passwordhash, mysqli_real_escape_string($connect, $_POST['password']));
 $loginquery = "SELECT * FROM usuarios WHERE email='$email' AND password='$password' LIMIT 1";
-$result = mysqli_query($connect, $loginquery) or die(mysqli_error($connect));
+$result = mysqli_query($connect, $loginquery) or die(mysqli_error());
 $row = mysqli_num_rows($result);
 if ($row === 1) {
     $data = mysqli_fetch_array($result);
@@ -17,10 +17,10 @@ if ($row === 1) {
             $_SESSION['email'] = $data['email'];
             $_SESSION['kid'] = $data['keywords_id'];
             $_SESSION['priv'] = $data['privileges'];
-            $id = $_SESSION['id'];
-            $updatelogints = "UPDATE 'usuarios' SET 'last_login_date'=CURRENT_TIMESTAMP WHERE 'id'=$id LIMIT 1";
+            $id = $_SESSION['id']
+            $updatelogints = "UPDATE `usuarios` SET `last_login_date`=CURRENT_TIMESTAMP WHERE `id`=$id LIMIT 1";
             //die(var_dump($updatelogints));
-            mysqli_query($connect, $updatelogints) or die(mysqli_error($connect));
+            mysqli_query($connect, $updatelogints) or die(mysqli_error());
             header("location: index.php");
         } else {
             header("Location: index.php?login=failed&reason=suspended");
