4991f140e9f698f03b8e275996fbf19be7753989
Axel Wdoviak <kooltheoutsider@gmail.com>
2019-01-14 16:38:00 -0300

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ include("config/mysql_connection.php");
 include("config/parameters.php");
 include("config/site_based_checks.php");
 $email = mysqli_real_escape_string($connect, $_POST['username']);
-$password = password_encrypt(mysqli_real_escape_string($connect, $_POST['password']), $passwordhash);
+$password = password_encrypt($passwordhash, mysqli_real_escape_string($connect, $_POST['password']));
 $loginquery = "SELECT * FROM usuarios WHERE email='$email' AND password='$password' LIMIT 1";
 $result = mysqli_query($connect, $loginquery) or die(mysqli_error());
 $row = mysqli_num_rows($result);
