3b3c2ae330b8c68a438f29f0e0213bd6da2ac98c
Axel Wdoviak <kooltheoutsider@gmail.com>
2019-01-14 15:46:23 -0300

    Update register.php

--- register.php
+++ register.php
@@ -7,13 +7,13 @@ $descripcion = $_POST['descripcion'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $confirm_password = $_POST['confirm_password'];
-//$keywords_id = createUniqueKeywordId();
+$keywords_id = createUniqueKeywordId();
 if ( comparepassword($password, $confirm_password) == true ) {
     $passtosave = password_encrypt($passwordhash, $_POST['password']);
 } else {
     return $error10;
 }
-$registrationquery = "INSERT INTO services SET fullname='$fullname', descripcion='$descripcion', email='$email', password='$passtosave'";
+$registrationquery = "INSERT INTO usuarios SET fullname='$fullname', descripcion='$descripcion', keywords_id=$keywords_id, email='$email', password='$passtosave'";
 mysqli_query($connect, $registrationquery) or die(mysqli_error($connect));
 echo "All goooood!";
 ?>
