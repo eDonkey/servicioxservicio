8d2dc03ee4f84625fd50065e6bf9719863e80e7f
Axel Wdoviak <kooltheoutsider@gmail.com>
2019-02-06 00:40:08 -0300

    Update new_profile.js

--- js/new_profile.js
+++ js/new_profile.js
@@ -4,6 +4,9 @@ function showApiKey(apikey) {
     console.log("DISPLAYING APIKEY");
     input.disabled="true";
     console.log("DISABLING INPUT");
+    input.value.select();
+    document.execCommand("copy");
+    console.log("VALUE COPIED");
 //    div = document.getElementById(divid);
 //    div.style.visibility = "visible";
 //    div.style.opacity = 1;
