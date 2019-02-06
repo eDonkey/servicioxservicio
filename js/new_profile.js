846b156f5d957248a675f81d53763400ff21eef1
Axel Wdoviak <kooltheoutsider@gmail.com>
2019-02-06 00:36:26 -0300

    Update new_profile.js

--- js/new_profile.js
+++ js/new_profile.js
@@ -1,6 +1,9 @@
 function showApiKey(apikey) {
-    input = document.getElementById("apikey").value = apikey;
+    input = document.getElementById("apikey");
+    input.value = apikey;
     console.log("DISPLAYING APIKEY");
+    input.disabled="true";
+    console.log("DISABLING INPUT");
 //    div = document.getElementById(divid);
 //    div.style.visibility = "visible";
 //    div.style.opacity = 1;
